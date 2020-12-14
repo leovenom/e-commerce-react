7032b024e132d8bef7a6e071eba565676f7ac667
leovenom <leonardt@oputlook.com>
2020-12-14 19:02:35 +0100

    added commerce.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import Products from  './components/Products/Products';
+import { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+
+// import Products from  './components/Products/Products';
+// import Navbar from  './components/Navbar/Navbar';
+import { Products, Navbar } from './components';
+
 
 //rafce
 import React from 'react'
 
 const App = () => {
+  const [products, SetProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data }  = await commerce.products.list();
+    SetProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  },[]);
+
   return (
     <div>
-      E-commerce
-      <Products/>
+      <Navbar />
+      <Products products={products} />
     </div>
   )
 }
