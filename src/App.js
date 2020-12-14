ad545754ba08d36ee78ab06136db43a2197d4a16
leovenom <leonardt@oputlook.com>
2020-12-14 20:50:36 +0100

    added cart

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { commerce } from './lib/commerce';
 
 // import Products from  './components/Products/Products';
 // import Navbar from  './components/Navbar/Navbar';
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 
 //rafce
@@ -12,20 +12,35 @@ import React from 'react'
 
 const App = () => {
   const [products, SetProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data }  = await commerce.products.list();
     SetProducts(data);
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart =  async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   },[]);
+
+  console.log(cart)
 
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items}/>
+      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+      <Cart cart={cart} />
     </div>
   )
 }
