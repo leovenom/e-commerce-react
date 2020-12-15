59b8f4b32aeefcdffb14eaf9c9a75d3ec2d67e41
leovenom <leonardt@oputlook.com>
2020-12-15 12:03:58 +0100

    function + - and remove all

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem'
+import { Link } from 'react-router-dom';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
-    <Typography variant='subtitle1'>You have no  items in your shpping cart, start adding some!</Typography>
+    <Typography variant='subtitle1'>You have no  items in your shpping cart, start adding some,
+      <Link to='/' className={classes.link} >Start adding some</Link>!
+    </Typography>
   );
   
   const FilledCart = () => (
@@ -15,16 +18,16 @@ const Cart = ({ cart }) => {
     <Grid container spacing={3}>
       {cart.line_items.map((item) => (
         <Grid item xs={12} sm={4} key={item.id}>
-          <CartItem  item={item}/>
+          <CartItem  item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>
         </Grid>
       ))}
     </Grid>
-    <div className={classes.cartDetails} >
+    <div className={classes.cardDetails} >
       <Typography variant='h4'>
         SubTotal: {cart.subtotal.formatted_with_symbol}
       </Typography>
       <div>
-        <Button className={classes.emptyButton} size='large' type='Button' variant='contained' color='secondary' >Empty Cart</Button>
+        <Button className={classes.emptyButton} size='large' type='Button' variant='contained' color='secondary' onClick={handleEmptyCart} >Empty Cart</Button>
         <Button className={classes.checkoutButton} size='large' type='Button' variant='contained' color='primary' >Checkout</Button>
       </div>
 
